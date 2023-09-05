29bd5d3c173577c9c868f869e6d6382ae0801740
Pedro Oliveira <Pedroaolive@gmail.com>
2023-09-05 19:44:21 -0300

    feat: add Jwt and security authentication to generate a token

---
+++ src/main/java/com/sistema/exames/controllers/AuthenticationController.java
@@ -0,0 +1,56 @@
+package com.sistema.exames.controllers;
+
+import com.sistema.exames.configs.JwtUtils;
+import com.sistema.exames.entities.JwtRequest;
+import com.sistema.exames.entities.JwtResponse;
+import com.sistema.exames.services.implementation.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @PostMapping("/generate-token")
+    public ResponseEntity<?> generarToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+        try{
+            autenticar(jwtRequest.getUsername(),jwtRequest.getPassword());
+        }catch (Exception exception){
+            exception.printStackTrace();
+            throw new Exception("Usuario não encontrado!");
+        }
+
+        UserDetails userDetails =  this.userDetailsService.loadUserByUsername(jwtRequest.getUsername());
+        String token = this.jwtUtils.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    private void autenticar(String username,String password) throws Exception {
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username,password));
+        }catch (DisabledException exception){
+            throw  new Exception("USUáRIO DESABILITADO " + exception.getMessage());
+        }catch (BadCredentialsException e){
+            throw  new Exception("Credenciais inválidas " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/sistema/exames/services/implementation/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.sistema.exames.services.implementation;
+
+import com.sistema.exames.entities.User;
+import com.sistema.exames.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Usuário não encontrado!");
+        }
+
+        return user;
+    }
+}
